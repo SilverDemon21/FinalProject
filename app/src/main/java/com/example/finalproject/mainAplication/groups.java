c4b324f522a033c8ed7d8dfc5e30869f3e2351f3
SilverDemon21 <danielserebro21@gmail.com>
2025-01-31 12:45:07 +0200

    not committed error

--- app/src/main/java/com/example/finalproject/mainAplication/groups.java
+++ app/src/main/java/com/example/finalproject/mainAplication/groups.java
@@ -2,8 +2,17 @@ package com.example.finalproject.mainAplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,10 +25,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class groups extends AppCompatActivity {
 
+    private Button btnCreateGroup;
+    private boolean itemIsSelected = false;
+    private String selectedOption;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+
+        btnCreateGroup = findViewById(R.id.btnCreateGroup);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationBarGroups);
 
@@ -49,5 +65,54 @@ public class groups extends AppCompatActivity {
 
 
         });
+
+
+        btnCreateGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater inflater = LayoutInflater.from(groups.this);
+                View dialogView = inflater.inflate(R.layout.dialog_create_new_group, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(groups.this);
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+
+                EditText etGroupName = dialogView.findViewById(R.id.etGroupName);
+                Button btnCreateGroup = dialogView.findViewById(R.id.btnCreateGroup);
+                Spinner spinner = dialogView.findViewById(R.id.spinnerGroupType);
+
+                String[] options = {"Parents Mode", "Friends Mode"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(groups.this, android.R.layout.simple_spinner_dropdown_item, options);
+                spinner.setAdapter(adapter);
+
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        selectedOption = options[i];
+                        itemIsSelected = true;
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+                btnCreateGroup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sendRequestToAdmin(etGroupName.getText().toString().trim(), selectedOption);
+                        Toast.makeText(groups.this, "Your request to open new group was redirected to the admin")
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
+
+
+
     }
 }
